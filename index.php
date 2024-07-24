971f9bcf7c9a6bd2bc34c9445216bad55c0503b0
Sirlife <simone@freedome.it>
2024-07-24 15:58:37 +0200

    feat: add dynamic data table

--- index.php
+++ index.php
@@ -1,71 +1,143 @@
 <?php
 
-    $hotels = [
+$hotels = [
 
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
 
-    ];
+];
 
-?> 
+?>
 
 <!-- Stampare tutti i nostri hotel con tutti i dati disponibili. -->
- <!DOCTYPE html>
- <html lang="en">
- <head>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
- </head>
- <body>
-    <div>   
-        <!-- facciamo un forEach per iterare su ogni elemento del array -->
-         <!-- Como nuestra estructura es multi, no podemos lanzar todo con un simple forEach, hay que aprovechar la palabra clave para iterar sobre cada objeto del array -->
-        <?php foreach($hotels as $hotel): ?>
-            <!-- Aqui tomamos palabra clave mas el nombre del hotel -->
-            <h2><?php echo $hotel['name']; ?></h2>
-            <!-- Aqui tomamos palabra clave mas la descripcion del hotel -->
-            <p><?php echo $hotel['description']; ?></p>
-            <!-- Aqui tomamos palabra clave mas y la propiedad parking del hotel e imponemos un ternario ? 'Yes' : 'No', parking tiene valores Booleanos, si el parking es True, entonces el output en pantalla sera Yes, y lo mismo con No. -->
-            <p>Parking: <?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></p>
-            <!-- Aqui tomamos palabra clave mas la puntuacion del hotel -->
-            <p>Vote: <?php echo $hotel['vote']; ?></p>
-            <!-- Aqui tomamos palabra clave mas la distancia en km del hotel -->
-            <p>Distance to center: <?php echo $hotel['distance_to_center']; ?> km</p>
-            <hr>
-        <?php endforeach; ?></div>
- </body>
- </html>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>PHP Hotel</title>
+</head>
+
+<body>
+
+<section class="container col-10">
+    <h3>PHP Hotel</h3>
+    <form>
+        <div>
+        <label>
+            <input type="radio" name="voto" value="0">
+            Solo con parcheggio
+        </label>
+        </div>
+        <div>
+            <span><h5>Voto</h5></span>
+            <label>
+                <input type="radio" name="voto" value="1">
+                1
+            </label>
+            <label>
+                <input type="radio" name="voto" value="2">
+                2
+            </label>
+            <label>
+                <input type="radio" name="voto" value="3">
+                3
+            </label>
+            <label>
+                <input type="radio" name="voto" value="4">
+                4
+            </label>
+            <label>
+                <input type="radio" name="voto" value="5">
+                5
+            </label>
+        </div>
+        <button type="button" class="btn btn-primary">Filtra</button>
+    </form>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col"> <h3>Nome</h3></th>
+                <th scope="col"><h3>Descrizione</h3></th>
+                <th scope="col"> <h3>Parcheggio</h3></th>
+                <th scope="col"><h3>Voto</h3></th>
+                <th scope="col"><h3>Distanza</h3></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row"><?php echo $hotels[0]['name']; ?></th>
+                <td><?php echo $hotels[0]['description']; ?></td>
+                <td><?php echo $hotels[0]['parking'] ? 'Yes' : 'No'; ?></td>
+                <td><?php echo $hotels[0]['vote']; ?></td>
+                <td><?php echo $hotels[0]['distance_to_center']; ?></td>
+            </tr>
+            <tr>
+                <th scope="row"><?php echo $hotels[1]['name']; ?></th>
+                <td><?php echo $hotels[1]['description']; ?></td>
+                <td><?php echo $hotels[1]['parking'] ? 'Yes' : 'No'; ?></td>
+                <td><?php echo $hotels[1]['vote']; ?></td>
+                <td><?php echo $hotels[1]['distance_to_center']; ?></td>
+            </tr>
+            <tr>
+                <th scope="row"><?php echo $hotels[2]['name']; ?></th>
+                <td><?php echo $hotels[2]['description']; ?></td>
+                <td><?php echo $hotels[2]['parking'] ? 'Yes' : 'No'; ?></td>
+                <td><?php echo $hotels[2]['vote']; ?></td>
+                <td><?php echo $hotels[2]['distance_to_center']; ?></td>
+            </tr>
+            <tr>
+                <th scope="row"><?php echo $hotels[3]['name']; ?></th>
+                <td><?php echo $hotels[3]['description']; ?></td>
+                <td><?php echo $hotels[3]['parking'] ? 'Yes' : 'No'; ?></td>
+                <td><?php echo $hotels[3]['vote']; ?></td>
+                <td><?php echo $hotels[3]['distance_to_center']; ?></td>
+            </tr>
+            <tr>
+                <th scope="row"><?php echo $hotels[4]['name']; ?></th>
+                <td><?php echo $hotels[4]['description']; ?></td>
+                <td><?php echo $hotels[4]['parking'] ? 'Yes' : 'No'; ?></td>
+                <td><?php echo $hotels[4]['vote']; ?></td>
+                <td><?php echo $hotels[4]['distance_to_center']; ?></td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+</body>
+
+</html>
